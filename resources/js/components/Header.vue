403a92266fd50f23e81ed07186625a8387849567
Roberto <robertoanfuso98@gmail.com>
2021-09-21 16:25:25 +0200

    first commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <router-link class="nav-link" :to="{ name: 'home' }">Boolpress</router-link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name: 'contatti' }">Contatti</router-link>
+                        </li>
+                        <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name: 'post' }">Posts</router-link>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
